1376ea45cd64a63280a5b8ee5b323c3d447eb5c2
THP1993 <tannerheitzphoto@gmail.com>
2025-08-24 14:19:59 -0500

    promise help

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-document.querySelector('.title').innerHTML = "hello world"
+
+function getVideo (subscriptionStatus) {
+return new Promise ( (resolve, reject) => {
+
+ if (subscriptionStatus === "VIP") {
+    resolve("Show video")} 
+
+ else if (subscriptionStatus === "FREE") {
+    resolve("show trailer") } 
+
+  else {
+    reject("no video")} 
+ 
+} )
+
+}
+
+async function main() {
+    const status = await getSubcriptionStatus ();
+    statusRef.innerHTML = status;
+    console.log(getVideo(status))
+}
+
+main();
+
+
+
