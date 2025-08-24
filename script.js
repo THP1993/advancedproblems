fbc0ef1c6d2dfea0a629aec5fd226952b8f0b05d
THP1993 <tannerheitzphoto@gmail.com>
2025-08-24 14:28:03 -0500

    still getting error

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+const statusRef = document.querySelector(".status");
+
+function getSubscriptionStatus() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve("VIP");
+
+        }, 2000);
+    });
+}
+
+
 
 function getVideo (subscriptionStatus) {
 return new Promise ( (resolve, reject) => {
